719c700db87737abff0219bb8c9a88ab7f5fd1d4
Ahmet Eyüp Şengün <asengun8@gmail.com>
2025-07-06 04:31:36 +0300

    routing alt yapısı kuruldu #1

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+if(process.env.NODE_ENV !="production")
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,12 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   console.log("Ben app.js te tanımlanan bir middlewarerim");
+  next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api',require('./routes/index'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,11 +36,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
